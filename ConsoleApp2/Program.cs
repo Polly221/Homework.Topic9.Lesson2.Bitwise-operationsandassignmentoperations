e36693d51ab47f98abe900b2795ae03dc4b93d98
Polly221 <132838293+Polly221@users.noreply.github.com>
2023-05-12 19:50:01 +0200

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,17 @@
-﻿//программа для инверсии
-Console.WriteLine("Введите число для инверсии:");
+//1.программа для инверсии
+Console.Write("Введите число для инверсии: ");
 long n = long.Parse(Console.ReadLine());
-Console.WriteLine("Результат:"+~n);
+Console.WriteLine("Результат: "+~n);
+//2.программа для сдвига числа влево
+Console.Write("Введите число для сдвига влево : ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Результат: "+(a<<1));
+//2.программа для сдвига числа вправо
+Console.Write("Введите число для сдвига вправо : ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Результат: " + (b >> 1));
+//3.программа для присваивания
+Console.Write("Введите число для присваивания : ");
+int c = int.Parse(Console.ReadLine());
+int result = c= 2487;
+Console.WriteLine("Результат: "+result);
